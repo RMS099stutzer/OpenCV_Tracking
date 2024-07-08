cb0abca9a7d375553e6e22bed7d58559f818e903
shirokuma89dev <code@shirokuma89.dev>
2024-07-08 16:00:00 +0900

    ✏️ 2値化

--- realtime_camera_capture.py
+++ realtime_camera_capture.py
@@ -3,9 +3,11 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
-lower = np.array([0,0,0])
-upper = np.array([255,255,100])
+# 閾値を変えてね（HSV色空間、色、彩度、明るさ）
+lower = np.array([0, 0, 0])
+upper = np.array([255, 255, 100])
 
+# 2値化を無効化
 disable = False
 
 if cap.isOpened() == False:
@@ -17,20 +19,23 @@ if cap.isOpened():
     print("Width: " + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
     print("Height: " + str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
     print("FPS: " + str(cap.get(cv2.CAP_PROP_FPS)))
-          
+
     print("Press 'q' to quit")
 
-
-while cap.isOpened():
+while cap.isOpened():  # カメラが存在しますか？
     ret, frame = cap.read()
     if ret == True:
         if not disable:
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             frame_mask = cv2.inRange(hsv, lower, upper)
 
-            frame = cv2.bitwise_and(frame, frame, mask=frame_mask)
-        
-        cv2.imshow("Webcam Live", frame)
+            filtered = cv2.bitwise_and(frame, frame, mask=frame_mask)
+
+        # frame_mask: 2値化した画像
+        # frame: 元画像
+        # filtered: マスクをかけた画像
+
+        cv2.imshow("Webcam Live", frame_mask) #画面表示
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
