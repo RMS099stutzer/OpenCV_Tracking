9e443d0ad03b38ea08af460e3ee417ad72edc5d4
Shibainukawaii <2021d18@stu.gifu-nct.ac.jp>
2024-10-17 16:03:55 +0900

    server側のループの変更

--- socket_com/test_receive.py
+++ socket_com/test_receive.py
@@ -4,28 +4,35 @@ import socket
 SERVER_HOST = '169.254.13.242'  # 全てのネットワークインターフェースから接続を受け付ける
 SERVER_PORT = 5000               # クライアント側と同じポート番号
 
-def receive_file(save_path):
+def receive_file():
     # ソケットを作成し、TCP接続を設定
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind((SERVER_HOST, SERVER_PORT))
         server_socket.listen(1)
         print(f'Server listening on {SERVER_HOST}:{SERVER_PORT}')
         
-        conn, addr = server_socket.accept()
-        print(f'Connected by {addr}')
-        
-        # ファイルを受信して保存
         while True:
-            data = conn.recv(1024)
-            if not data:
-                break
+            conn, addr = server_socket.accept()
+            print(f'Connected by {addr}')
             
-            with open(save_path, 'ab') as file:  # 'ab'モードで追記
-                file.write(data)
-                
-            print(f'Received {len(data)} bytes.')
-
-        print('File reception completed.')
+            # ファイル名を受信
+            file_name = conn.recv(1024).decode('utf-8')
+            if not file_name:
+                print("No filename received.")
+                conn.close()
+                continue
+            
+            # 新しいファイルを作成して受信
+            with open(file_name, 'ab') as file:  # 'ab'モードで追記
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    file.write(data)
+                    print(f'Received {len(data)} bytes for file {file_name}.')
+            
+            print(f'File reception completed for {file_name}.')
+            conn.close()  # クライアントとの接続を閉じる
 
 if __name__ == "__main__":
-    receive_file('received.txt')  # 保存するファイルのパスを指定
+    receive_file()  # ファイルを受信する
